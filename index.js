2f1280bf0a9896dab50d6c770e6549b9ef630c9e
Paul R. Davidson <lastlife@gmail.com>
2018-06-17 15:33:48 +0200

    Front changes: - Added Handlebars template engine. - Added system to show the viewboard. - Added system to paginate big data from back. - Added simulation template.

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+"use strict";
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
@@ -10,16 +15,21 @@ require('string.prototype.startswith');
 app.use('/assets', express.static(__dirname + '/bower_components'));
 app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use('/statics', express.static(__dirname + '/statics'));
+app.use('/views', express.static(__dirname + '/views'));
 
 require('./routes')(app);
 
 // Init the server
-server.listen(3000, function () {
-    console.log('listening on *:3000');
+server.listen(port, hostname, function () {
+    console.log(`listening on http://${hostname}:${port}`);
 });
 
 // Socket.io connections
 io.on('connection', function (socket) {
+    // Plugin handler
+    socket.on("plugin", function(data) {
+        io.emit("plugin", data);
+    });
 
     // Message handler
     socket.on('message', function (msg) {

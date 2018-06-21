0c6547e9a32c9de92978ba87727f5c5a08873d62
tx2z <tx2z@caosmental.com>
2018-06-21 19:18:32 +0200

    small changes in weather plugin

--- plugins/weather/main.js
+++ plugins/weather/main.js
@@ -11,12 +11,12 @@ class plugin_weather {
     fire() {
         var self = this;
         this.call_api(function(value) {
-        	let weather_data = {
-        		"weather": value.weather[0].main,
-        		"weather_desc": value.weather[0].description,
-        		"weather_icon": value.weather[0].id,
-        		"temp" : value.main.temp
-        	};
+            let weather_data = {
+                "weather": value.weather[0].main,
+                "weather_desc": value.weather[0].description,
+                "weather_icon": value.weather[0].id,
+                "temp" : value.main.temp
+            };
             self.data = {
                 "id": "plugin_weather", 
                 "name": "El tiempo en Madrid️", 
@@ -29,9 +29,11 @@ class plugin_weather {
     }
 
     call_api(cb) {
-    	const owm_key  = '6216ac761ade705b1c9c59d69cee478f';
-		const owm_city = '6359304';
-        request('http://api.openweathermap.org/data/2.5/weather?units=metric&id='+ owm_city +'&appid=' + owm_key, {json: true}, (err, res, body) => {
+        const owm_key     = '6216ac761ade705b1c9c59d69cee478f'; 
+        const owm_city    = '6359304'; // Madrid, Madrid, Madrid pedazo de la españa en que nací
+        const owm_unit    = 'metric'; // Celsious
+        const owm_version = '2.5';
+        request('http://api.openweathermap.org/data/' + owm_version + '/weather?units=' + owm_unit + '&id='+ owm_city +'&appid=' + owm_key, {json: true}, (err, res, body) => {
             if (err) {
                 return console.log(err);
             }

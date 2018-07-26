50880d8a35698f977ffd47e88a927421da741b13
Aritz Iribarren Galarza <aritz.iribarren@scalefast.com>
2018-07-26 15:05:26 +0200

    added gallery plugin

---
+++ plugins/gallery/main.js
@@ -0,0 +1,45 @@
+var fs = require('fs');
+var path = require( 'path' );
+const plugin = require('../plugin');
+const shuffle = require('shuffle-array');
+
+class plugin_name {
+
+    constructor(config, queue) {
+        this.config = config;
+        this.queue = queue;
+    }
+    
+    fire() {
+        let statics = "statics/images/gallery/"
+        let media = __dirname+"/../../"+statics;
+        let photos = [];
+        let data_photos = [];
+        let photo_path;
+        fs.readdir( media, function( err, files ) {
+            if( err ) {
+                console.error( "Could not list the directory.", err );
+                process.exit( 1 );
+            } 
+            files.forEach( function( file, index ) {
+                photo_path = path.join( statics, file );
+                photos.push(photo_path);
+            } );
+            shuffle(photos);
+            let i = 0;
+            while (i<15) {
+                data_photos.push({
+                    "path1" : photos[i++],
+                    "path2" : photos[i++],
+                    "path3" : photos[i++]
+                });
+            }
+        } );
+        
+        this.data = this.config;
+        this.data.data = data_photos;
+        this.queue.push(this);
+    }
+}
+
+module.exports = plugin_name;
